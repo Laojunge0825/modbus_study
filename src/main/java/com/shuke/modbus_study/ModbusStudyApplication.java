5700f8f91d6d5af5316eb3f1b7994d44073448ca
pengcanjun <1642287765@qq.com>
2025-03-28 11:27:47 +0800

    初始化项目

---
+++ src/main/java/com/shuke/modbus_study/ModbusStudyApplication.java
@@ -0,0 +1,13 @@
+package com.shuke.modbus_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModbusStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModbusStudyApplication.class, args);
+	}
+
+}
